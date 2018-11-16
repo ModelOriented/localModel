e06cfd772eee0caf5eb1f1e0ce3bb5cb6eec5026
Mateusz Staniak <mtst@mstaniak.pl>
2018-11-16 23:13:28 +0100

    changes names of main function and arguments

--- R/approximations.R
+++ R/approximations.R
@@ -25,12 +25,13 @@ encode_data_frame <- function(explainer, data, prediction_quantiles) {
 
 #' Local surrogate model
 #'
-#' @param explainer DALEX explainer object.
-#' @param observation Instance to be explained.
+#' @param x an explainer created with function `DALEX2::explain()`
+#' @param new_observation an observation/observations to be explained.
+#'        Columns in should correspond to columns in the data element of explainer.
 #' @param size Number of instance to simulate.
 #' @param seed Argument to set.seed, if not specified, results will not be reproducible.
 #' @param response_family Family argument to glmnet function.
-#' @param n_rows Number of points, at which ceteris paribus curves will be calculated.
+#' @param n_points Number of points, at which ceteris paribus curves will be calculated.
 #' @param smoothness Span argument to loess function.
 #'
 #' @return glmnet model object
@@ -40,45 +41,49 @@ encode_data_frame <- function(explainer, data, prediction_quantiles) {
 #' @export
 #'
 
-local_approximation <- function(explainer, observation,
-                                size, seed = NULL,
-                                response_family =  "gaussian",
-                                n_points = nrow(explainer$data),
-                                smoothness = 0.3, ...) {
-  check_conditions(explainer$data, observation, size)
-  numerical_data <- dplyr::select_if(explainer$data, is.numeric)
-  categorical_data <- dplyr::select_if(explainer$data,
-                                       function(x) is.factor(x) | is.character(x))
+individual_surrogate_model <- function(x, new_observation,
+                                       size, seed = NULL,
+                                       response_family =  "gaussian",
+                                       kernel = gaussian_kernel,
+                                       n_points = nrow(x$data),
+                                       smoothness = 0.3, ...) {
+  check_conditions(x$data, new_observation, size)
+  numerical_data <- dplyr::select_if(x$data, is.numeric)
+  categorical_data <- dplyr::select_if(x$data,
+                                       function(y) is.factor(y) | is.character(y))
 
   whatif_curves <- lapply(
     colnames(numerical_data),
-    function(x) {
-      ceteris_curve <- ceterisParibus::ceteris_paribus(explainer,
-                                                       observation,
-                                                       variables = x,
-                                                       grid_points = n_points)[, c(x, "_yhat_")]
+    function(y) {
+      ceteris_curve <- ceterisParibus::ceteris_paribus(x,
+                                                       new_observation,
+                                                       variables = y,
+                                                       grid_points = n_points)[, c(y, "_yhat_")]
       ceteris_curve[, 2] <- suppressWarnings(predict(loess(ceteris_curve[, 2] ~ ceteris_curve[, 1],
-                                          span = smoothness)))
+                                                           span = smoothness)))
       ceteris_curve
     })
 
   prediction_quantiles <- lapply(whatif_curves,
-                                 function(x) {
-                                   quantile(x$`_yhat_`, probs = c(1/3, 2/3))
+                                 function(y) {
+                                   quantile(y$`_yhat_`, probs = c(1/3, 2/3))
                                  })
 
-  encoded_observation <- encode_data_frame(explainer, dplyr::select_if(observation, is.numeric),
+  encoded_observation <- encode_data_frame(x,
+                                           dplyr::select_if(new_observation,
+                                                            is.numeric),
                                            prediction_quantiles)
   encoded_observation <- dplyr::bind_cols(encoded_observation,
-                                          dplyr::select_if(observation,
-                                                           function(x) !is.numeric(x)))
+                                          dplyr::select_if(new_observation,
+                                                           function(y) !is.numeric(y)))
 
-  encoded_data <- encode_data_frame(explainer, numerical_data,
+  encoded_data <- encode_data_frame(x,
+                                    numerical_data,
                                     prediction_quantiles)
 
   means_by_group <- vector("list", ncol(encoded_data))
-  for(x in 1:ncol(numerical_data)) {
-    means_by_group[[x]] <- tapply(numerical_data[, x], encoded_data[, x], mean)
+  for(y in 1:ncol(numerical_data)) {
+    means_by_group[[y]] <- tapply(numerical_data[, y], encoded_data[, y], mean)
   }
 
   encoded_data <- dplyr::bind_cols(encoded_data, categorical_data)
@@ -106,10 +111,10 @@ local_approximation <- function(explainer, observation,
     }
   }
 
-  model_response <- explainer$predict_function(explainer$model, to_predict)
+  model_response <- x$predict_function(x$model, to_predict)
   explorer <- list(data = similar,
                    target = model_response,
-                   explained_instance = observation)
+                   explained_instance = new_observation)
 
   model <- fit_explanation(explorer, kernel, response_family = response_family)
   class(model) <- c("live_explainer", class(model))
