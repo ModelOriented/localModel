20a3b164b8d095dd54ee32bd41f524d92fcd0385
Mateusz Staniak <mtst@mstaniak.pl>
2018-12-21 11:54:00 +0100

    changes interpretable inptus
    they are now the same for each output level

--- R/local_surrogate.R
+++ R/local_surrogate.R
@@ -1,20 +1,9 @@
 #' @importFrom stats as.formula coef model.matrix
 
-single_column_surrogate <- function(x, new_observation, size, seed = NULL,
+single_column_surrogate <- function(x, new_observation,
+                                    encoded_data,
+                                    size, seed = NULL,
                                     sampling = "uniform") {
-  feature_representations <- lapply(
-    colnames(new_observation),
-    function(colname) {
-      feature_representation(x,
-                             new_observation,
-                             colname)
-    }
-  )
-
-  encoded_data <- as.data.frame(feature_representations)
-  colnames(encoded_data) <- intersect(colnames(new_observation),
-                                      colnames(x$data))
-
   p <- ncol(encoded_data)
   simulated_data <- as.data.frame(
     lapply(encoded_data,
@@ -27,7 +16,7 @@ single_column_surrogate <- function(x, new_observation, size, seed = NULL,
                     as.data.frame(prop.table(table(column)))$Freq
                   })
 
-  if(!is.null(set.seed)) set.seed(seed)
+  if(!is.null(seed)) set.seed(seed)
   for(row_number in 1:size) {
     n_changes <- sample(1:p, 1)
     change_indices <- sample(1:p, n_changes)
@@ -86,8 +75,10 @@ single_column_surrogate <- function(x, new_observation, size, seed = NULL,
   simulated_data <- simulated_data[, sapply(simulated_data,
                                             function(col) length(unique(col)) > 1)]
   simulated_data[["y"]] <- 1
-  fitted_model <- glmnet::cv.glmnet(model.matrix(y ~., data =  simulated_data)[, -1],
-                                    predicted_scores,
+  model_mean <- mean(x$predict_function(x$model, x$data))
+  fitted_model <- glmnet::cv.glmnet(model.matrix(y ~ .,
+                                                 data =  simulated_data)[, -1],
+                                    predicted_scores - model_mean,
                                     alpha = 1)
   result <- as.data.frame(as.matrix(coef(fitted_model, lambda = "lambda.min")))
   result$variable <- rownames(result)
@@ -95,20 +86,24 @@ single_column_surrogate <- function(x, new_observation, size, seed = NULL,
   colnames(result)[1] <- "estimated"
   for(row_number in 2:nrow(result)) {
     result[row_number, "variable"] <- stringr::str_sub(result[row_number, "variable"],
-                                                       stringr::str_length(colnames(new_observation)[row_number - 1]) + 1)
-
+                                                       stringr::str_length(colnames(simulated_data)[row_number - 1]) + 1)
   }
-  result
+  rbind(
+    data.frame(estimated = model_mean,
+               variable = "(Model mean)"),
+    result
+  )
 }
 
 
 #' LIME-like explanations based on Ceteris Paribus curves
 #'
-#' @param x an explainer created with function DALEX2::explain().
+#' @param x an explainer created with the function DALEX2::explain().
 #' @param new_observation an observation to be explained. Columns in should correspond to columns in the data argument to x.
 #' @param size number of similar observation to be sampled.
 #' @param seed If not NULL, seed will be set to this value for reproducibility.
 #' @param sampling Parameter that controls sampling while creating new observations.
+#' @param grid_points Number of points to use while calculating Ceteris Paribus profiles.
 #'
 #' @return data.frame of class local_surrogate_explainer
 #'
@@ -130,8 +125,27 @@ single_column_surrogate <- function(x, new_observation, size, seed = NULL,
 #'
 
 individual_surrogate_model <- function(x, new_observation, size, seed = NULL,
-                                       sampling = "uniform") {
+                                       sampling = "uniform", grid_points = 101) {
+  x$data <- x$data[, intersect(colnames(x$data), colnames(new_observation))]
   try_predict <- x$predict_function(x$model, x$data[1:5, ])
+  predicted_names <- colnames(try_predict)
+  if(is.null(predicted_names))
+    predicted_names <- "yhat"
+
+  feature_representations <- lapply(
+    colnames(x$data),
+    function(column) {
+      feature_representation(x,
+                             new_observation,
+                             column,
+                             predicted_names,
+                             grid_points)
+    }
+  )
+  encoded_data <- as.data.frame(feature_representations)
+  colnames(encoded_data) <- intersect(colnames(new_observation),
+                                      colnames(x$data))
+
   if(!is.null(ncol(try_predict))) {
     explainer <- lapply(unique(colnames(try_predict)), function(unique_level) {
       internal_explainer <- x
@@ -139,7 +153,9 @@ individual_surrogate_model <- function(x, new_observation, size, seed = NULL,
         x$predict_function(model, newdata)[, unique_level]
 
       }
-      result <- single_column_surrogate(internal_explainer, new_observation,
+      result <- single_column_surrogate(internal_explainer,
+                                        new_observation,
+                                        encoded_data,
                                         size, seed, sampling)
       result[, "response"] <- unique_level
       result[, "predicted_value"] <- internal_explainer$predict_function(
@@ -151,6 +167,7 @@ individual_surrogate_model <- function(x, new_observation, size, seed = NULL,
   } else {
       explainer <- single_column_surrogate(
         x, new_observation,
+        encoded_data,
         size, seed, sampling
       )
       explainer[["response"]] <- ""
@@ -202,6 +219,7 @@ plot.local_surrogate_explainer <- function(x, ...) {
   )
 
   x <- x[x$estimated != 0, ]
+  x <- x[x$variable != "(Intercept)", ]
   x$estimated <- unlist(tapply(x$estimated,
                                x$response,
                                function(y) {
@@ -215,14 +233,15 @@ plot.local_surrogate_explainer <- function(x, ...) {
     )
   )
 
-  x <- x[x$variable != "(Intercept)", ]
+  x <- x[x$variable != "(Model mean)", ]
 
-  ggplot(x, aes(x = reorder(variable, -abs(estimated)),
+  ggplot(x, aes(x = reorder(variable, abs(estimated)),
                 y = estimated)) +
     theme_bw() +
     geom_hline(aes(yintercept = intercept),
                size = 1)  +
-    geom_pointrange(aes(ymin = intercept, ymax = estimated)) +
+    geom_pointrange(aes(ymin = intercept, ymax = estimated),
+                    size = 2) +
     facet_wrap(~response, ncol = 1, scales = "free_y") +
     coord_flip() +
     ylab("Estimated effect") +
