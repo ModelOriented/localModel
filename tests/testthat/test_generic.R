04a22a88b8b5f7f302c676f47b41676094c32039
Mateusz Staniak <mtst@mstaniak.pl>
2018-12-21 14:15:50 +0100

    adds test for plot

--- tests/testthat/test_generic.R
+++ tests/testthat/test_generic.R
@@ -1,7 +1,8 @@
 context("Test generics")
 
 testthat::expect_silent({
- plot(local_model_explainer_ap)
+  plot(local_model_explainer_ap)
+  plot(local_model_explainer_ap, geom = "bar")
 })
 
 testthat::expect_output({
