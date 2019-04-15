d03547104efa153df1bb4b77290f45a3efbcedb9
harell <harel@harmonic.co.nz>
2019-04-16 10:41:38 +1200

    corrections

--- tests/testthat/setup-xyz.R
+++ tests/testthat/setup-xyz.R
@@ -4,15 +4,6 @@
 ## Clean up the envrionment
 suppressWarnings(rm(HR, apartments))
 
-## Get project working directory
-path_project <- getwd()
-while (!assertive::is_empty(grep("test", path_project)))
-  path_project <- dirname(path_project)
-
-## Delete the template dirctory
-unlink(file.path(path_project, "temp"), recursive = TRUE, force = TRUE)
-rm(path_project)
-
 # Get the data
 data("HR", package = "DALEX")
 data("apartments", package = "DALEX")
